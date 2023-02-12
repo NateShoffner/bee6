a696d5e5c5c27ea0a171ea9a589b0167bcf6a275
Nate Shoffner <nate.shoffner@gmail.com>
2023-02-12 03:25:16 -0500

    Detect typing

--- cogs/luminewake/luminewake.py
+++ cogs/luminewake/luminewake.py
@@ -21,6 +21,11 @@ class LumineWake(commands.Cog):
         self.logger.info('LumineWake ready')
 
     @commands.Cog.listener()
+    async def on_typing(self, channel: discord.TextChannel, user: discord.User, when: datetime.datetime):
+        if self.config['detect_typing']:
+            await self.wake_if_needed(user, channel)
+
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild.id != 553590545801281541:
             return
@@ -29,23 +34,26 @@ class LumineWake(commands.Cog):
             await self.send_wake(message.author, message.channel)
             return
 
-        babe_config = self.get_babe_config(message.author)
-        # got ourselves a babe to wakeup
+        await self.wake_if_needed(message)
+
+    async def wake_if_needed(self, user: discord.User, channel: discord.TextChannel):
+        babe_config = self.get_babe_config(user)
+
+        # got ourselves a babe to wake up
         if babe_config:
             now = self.get_weirdo_time()
             last_wake_val = babe_config['last_wake']
             needs_woke = last_wake_val is None or (now.date() != datetime.fromisoformat(last_wake_val).date())
 
             if needs_woke:
-                self.logger.info(f'Waking {message.author.display_name} at {now}')
+                self.logger.info(f'Waking {user.display_name} at {now}')
 
                 fixed_channel_id = babe_config['channel']
-                channel = self.bot.get_channel(fixed_channel_id) if fixed_channel_id else message.channel
+                channel = self.bot.get_channel(fixed_channel_id) if fixed_channel_id else channel
 
-                await self.send_wake(message.author, channel)
+                await self.send_wake(user, channel)
                 babe_config['last_wake'] = now.isoformat()
                 save_config(self.config)
-
 
     async def send_wake(self, user: discord.User, channel: discord.TextChannel):
         await channel.typing()
